a7d27b66604ef220843d9f91c4134739c4f7231e
Wybe Westra <wybe@ruurdwestra.nl>
2026-02-13 19:09:39 +0100

    List is separated in printable and not

--- ui/model_info_view.gd
+++ ui/model_info_view.gd
@@ -2,7 +2,11 @@ extends PanelContainer
 
 var model: Model = null
 
-@onready var files_list := %FilesList
+## These extensions can be put on the 3d printer. Others can't.
+var printable_file_extensions := [".stl", ".3mf"]
+
+@onready var printables_list := %PrintablesList
+@onready var rest_list := %RestList
 
 func display_model(new_model: Model):
 	model = new_model
@@ -11,11 +15,33 @@ func display_model(new_model: Model):
 	if model.cover_image != null:
 		%CoverImage.icon = model.cover_image
 	
-	for child in files_list.get_children():
-		files_list.remove_child(child)
+	for child in printables_list.get_children():
+		printables_list.remove_child(child)
+		child.queue_free()
+	
+	for child in rest_list.get_children():
+		rest_list.remove_child(child)
 		child.queue_free()
 	
 	for file in model.files:
-		var label := Label.new()
-		label.text = file
-		files_list.add_child(label)
+		if _is_file_printable(file):
+			_add_file_to_printables(file)
+		else:
+			_add_file_to_rest_list(file)
+
+func _add_file_to_printables(file: String):
+	var label := Label.new()
+	label.text = file
+	printables_list.add_child(label)
+
+func _add_file_to_rest_list(file: String):
+	var label := Label.new()
+	label.text = file
+	rest_list.add_child(label)
+
+func _is_file_printable(file: String) -> bool:
+	for extension in printable_file_extensions:
+		if file.ends_with(extension):
+			return true
+	
+	return false
