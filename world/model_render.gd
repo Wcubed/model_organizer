1065cdde448f788d312f72f6740910721faedad7
Wybe Westra <wybe@ruurdwestra.nl>
2026-02-13 21:11:35 +0100

    Models correctly rotated

--- world/model_render.gd
+++ world/model_render.gd
@@ -1,7 +1,11 @@
 extends Node3D
 
+# Z is 'up' in most models, so the camera angle is a bit weird for godot.
+var anchor_start_angle := Vector3(45, -45, -45)
+
 @onready var model := %Model
 @onready var camera := %Camera3D
+@onready var camera_anchor := %CameraAnchor
 
 func show_model(mesh: ArrayMesh):
 	model.mesh = mesh
@@ -12,3 +16,4 @@ func show_model(mesh: ArrayMesh):
 	model.position = -aabb.get_center()
 	
 	camera.position.z = aabb.get_longest_axis_size()
+	camera_anchor.rotation_degrees = anchor_start_angle
