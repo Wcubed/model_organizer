dfcec09272f4299fc7a0ee4eb2aa9866141a2190
Wybe Westra <wybe@ruurdwestra.nl>
2026-02-13 15:52:06 +0100

    Basic model list

--- ui/main.gd
+++ ui/main.gd
@@ -1,21 +1,31 @@
 extends PanelContainer
 
 @onready var folder_dialog := $FolderDialog
+@onready var model_cards := %ModelCards
+
+var model_card_scene := preload("res://ui/model_card.tscn")
 
 ## List of extensions that will be ignored when checking if a folder contains models.
 var ignored_extensions := PackedStringArray([".zip", ".rar", ".orynt3d"])
 var library_dir: String = ""
+var model_dirs := PackedStringArray()
 
 func _ready() -> void:
 	scan_library()
 
 func scan_library():
-	var model_dirs := PackedStringArray()
-	scan_directory(library_dir, model_dirs)
+	if library_dir.is_empty():
+		refresh_model_cards()
+		return
 	
-	print(model_dirs)
+	var found_model_dirs := PackedStringArray()
+	scan_directory(library_dir, found_model_dirs)
+	
+	model_dirs = found_model_dirs
+	
+	refresh_model_cards()
 
-func scan_directory(path: String, model_dirs: PackedStringArray):
+func scan_directory(path: String, found_model_dirs: PackedStringArray):
 	var dir = DirAccess.open(path)
 	if !dir:
 		return
@@ -26,13 +36,23 @@ func scan_directory(path: String, model_dirs: PackedStringArray):
 	
 	if files.size() > 0:
 		# Model directory
-		model_dirs.append(path)
+		found_model_dirs.append(path)
 	else:
 		# Might contain sub directories
 		var subdirs = dir.get_directories()
 		for subdir in subdirs:
 			var new_path = "%s/%s" % [path, subdir]
-			scan_directory(new_path, model_dirs)
+			scan_directory(new_path, found_model_dirs)
+
+## Clears the model cards and displays the currently found models.
+func refresh_model_cards():
+	for card in model_cards.get_children():
+		model_cards.remove_child(card)
+		card.queue_free()
+	
+	for model in model_dirs:
+		var new_card = model_card_scene.instantiate()
+		model_cards.add_child(new_card)
 
 ## Returns true if the file should be included, false otherwise.
 func _filter_ignored_files(file: String) -> bool:
