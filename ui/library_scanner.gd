85686b7ee7366d47bc1d26960f7e89b1d5707fbe
Wybe Westra <wybe@ruurdwestra.nl>
2026-02-17 14:49:10 +0100

    Multithreaded library scanning.

--- ui/library_scanner.gd
+++ ui/library_scanner.gd
@@ -4,9 +4,15 @@ extends PanelContainer
 signal library_scan_complete(found_models: Array[Model])
 
 var scan_thread: Thread = null
-## After being detected, the models are scanned in another thread.
-var model_scan_thread: Thread = null
+
+## After being detected, the models are scanned in multiple threads.
+var model_scan_threads: Array[Thread] = []
 var model_scan_queue: Array[Model] = []
+## After being processed, the models are appended to the found models array.
+## We cannot create and delete references to the Model in multiple threads
+## at the same time, because RefCounted _isnt_ atomic!
+## So we make the model scan thread responsible for handling the references.
+var found_models: Array[Model] = []
 var run_model_scan_thread := false
 
 ## Updated by the background thread, read by the main thread.
@@ -30,8 +36,10 @@ func _process(_delta: float) -> void:
 	
 	if !scan_thread.is_alive() && model_scan_queue.is_empty():
 		run_model_scan_thread = false
-		var found_models = scan_thread.wait_to_finish()
-		model_scan_thread.wait_to_finish()
+		scan_thread.wait_to_finish()
+
+		for thread in model_scan_threads:
+			thread.wait_to_finish()
 		
 		library_scan_complete.emit(found_models)
 		print("Scan took %.1fs" % time_taken)
@@ -43,6 +51,7 @@ func _process(_delta: float) -> void:
 ## Starts the library scan in the background.
 func background_scan_library(library_dir: String):
 	show()
+	found_models.clear()
 	amount_found = 0
 	amount_processed = 0
 	%AmountLabel.text = ""
@@ -54,15 +63,17 @@ func background_scan_library(library_dir: String):
 	scan_thread.start(_thread_scan_library.bind(library_dir))
 	
 	run_model_scan_thread = true
-	model_scan_thread = Thread.new()
-	model_scan_thread.start(_thread_scan_queued_models)
+	
+	# Arbitrary number of threads.
+	for i in 5:
+		var new_thread := Thread.new()
+		new_thread.start(_thread_scan_queued_models)
+		model_scan_threads.append(new_thread)
 
 
-func _thread_scan_library(library_dir: String) -> Array[Model]:
-	var found_models: Array[Model] = []
-	_scan_directory(library_dir, library_dir, found_models)
+func _thread_scan_library(library_dir: String):
+	_scan_directory(library_dir, library_dir)
 	found_models.sort_custom(_sort_models_by_name)
-	return found_models
 
 
 func _thread_scan_queued_models():
@@ -73,10 +84,11 @@ func _thread_scan_queued_models():
 			OS.delay_msec(1)
 		else:
 			next_model.scan_directory()
+			found_models.append(next_model)
 			amount_processed += 1
 
 
-func _scan_directory(path: String, library_dir: String, found_models: Array[Model]):
+func _scan_directory(path: String, library_dir: String):
 	var dir = DirAccess.open(path)
 	if !dir:
 		return
@@ -89,7 +101,6 @@ func _scan_directory(path: String, library_dir: String, found_models: Array[Mode
 		# This is a model directory
 		var new_model = Model.new(path, library_dir)
 		model_scan_queue.append(new_model)
-		found_models.append(new_model)
 		amount_found += 1
 	else:
 		# Might contain sub directories
@@ -101,13 +112,12 @@ func _scan_directory(path: String, library_dir: String, found_models: Array[Mode
 				# Thingyverse.
 				var new_model = Model.new(path, library_dir)
 				model_scan_queue.append(new_model)
-				found_models.append(new_model)
 				amount_found += 1
 				return
 		
 		for subdir in subdirs:
 			var new_path = "%s/%s" % [path, subdir]
-			_scan_directory(new_path, library_dir, found_models)
+			_scan_directory(new_path, library_dir)
 
 
 ## Returns true if the file should be included, false otherwise.
