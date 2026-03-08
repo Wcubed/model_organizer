2443c3152430300d055dcda5e2095f0b0f005a5f
Wybe Westra <wybe@ruurdwestra.nl>
2026-03-08 14:48:49 +0100

    Attempt at calculating normals ourselves.

--- rust_native/src/lib.rs
+++ rust_native/src/lib.rs
@@ -30,35 +30,52 @@ impl StlLoader {
     #[func]
     fn load_from_file(path: GString) -> Variant {
         let Ok(mut file) = GFile::open(&path, ModeFlags::READ) else {
-            godot_print!("File open error");
             return 0.to_variant();
         };
 
         let result: Result<stl_io::IndexedMesh, _> = stl_io::read_stl(&mut file);
 
-        godot_print!("Loaded");
-
         match result {
             Ok(stl) => {
-                // Each face has 3 vertices, so we know the capacity needed.
+                // Each face has 3 vertices, so we know the capacities needed.
                 let mut vertex_array = Vec::with_capacity(stl.vertices.len());
+                let mut normal_array = vec![Vector3::ZERO; stl.vertices.len()];
                 let mut index_array = Vec::with_capacity(stl.faces.len() * 3);
 
                 for vertex in stl.vertices {
                     vertex_array.push(Vector3::new(vertex[0], vertex[1], vertex[2]));
                 }
                 for face in stl.faces {
+                    // Calculate normal for each face. Because those in the file are not always correct.
+                    let idx_a = face.vertices[0];
+                    let idx_b = face.vertices[1];
+                    let idx_c = face.vertices[2];
+                    let a = vertex_array[idx_a];
+                    let b = vertex_array[idx_b];
+                    let c = vertex_array[idx_c];
+
+                    let ab = b - a;
+                    let ac = c - a;
+                    let normal = ac.cross(ab);
+
                     for index in face.vertices {
                         index_array.push(index as i32);
+                        normal_array[index] += normal;
                     }
                 }
 
+                // Normalize the normals.
+                for normal in &mut normal_array {
+                    *normal = normal.normalized();
+                }
+
                 let packed_vertices = PackedVector3Array::from(vertex_array);
+                let packed_normals = PackedVector3Array::from(normal_array);
                 let packed_indices = PackedInt32Array::from(index_array);
 
                 let arrays = Array::from(&[
                     packed_vertices.to_variant(), // Vertices
-                    Variant::nil(),               // Normals
+                    packed_normals.to_variant(),  // Normals
                     Variant::nil(),
                     Variant::nil(),
                     Variant::nil(),
@@ -73,11 +90,8 @@ impl StlLoader {
                 ]);
 
                 let mut output = ArrayMesh::new_gd();
-                godot_print!("go: {}", output.get_surface_count());
+
                 output.add_surface_from_arrays(PrimitiveType::TRIANGLES, &arrays);
-
-                godot_print!("{}", output.get_surface_count());
-
                 output.to_variant()
             }
             Err(_error) => 0.to_variant(),
