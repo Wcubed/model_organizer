b7b2d8d88911000f7991312e2bb4a8b2491c0d3b
Wybe Westra <wybe@ruurdwestra.nl>
2026-03-08 14:31:41 +0100

    Successful load, but no normals.

--- rust_native/src/lib.rs
+++ rust_native/src/lib.rs
@@ -1,7 +1,12 @@
-use std::io::Read;
-
-use godot::{classes::file_access::ModeFlags, prelude::*};
-use tinystl::StlData;
+use godot::{
+    classes::{
+        ArrayMesh,
+        file_access::ModeFlags,
+        mesh::{ArrayType, PrimitiveType},
+    },
+    obj::IndexEnum,
+    prelude::*,
+};
 
 struct RustNativeExtension;
 
@@ -21,22 +26,61 @@ impl IRefCounted for StlLoader {
 
 #[godot_api]
 impl StlLoader {
+    /// Returns ArrayMesh or an integer to indicate error.
     #[func]
-    fn load_from_file(path: GString) {
-        let Ok(file) = GFile::open(&path, ModeFlags::READ) else {
+    fn load_from_file(path: GString) -> Variant {
+        let Ok(mut file) = GFile::open(&path, ModeFlags::READ) else {
             godot_print!("File open error");
-            return;
+            return 0.to_variant();
         };
 
-        let result = StlData::read_buffer(file);
+        let result: Result<stl_io::IndexedMesh, _> = stl_io::read_stl(&mut file);
+
+        godot_print!("Loaded");
 
         match result {
             Ok(stl) => {
-                godot_print!("Success!");
+                // Each face has 3 vertices, so we know the capacity needed.
+                let mut vertex_array = Vec::with_capacity(stl.vertices.len());
+                let mut index_array = Vec::with_capacity(stl.faces.len() * 3);
+
+                for vertex in stl.vertices {
+                    vertex_array.push(Vector3::new(vertex[0], vertex[1], vertex[2]));
+                }
+                for face in stl.faces {
+                    for index in face.vertices {
+                        index_array.push(index as i32);
+                    }
+                }
+
+                let packed_vertices = PackedVector3Array::from(vertex_array);
+                let packed_indices = PackedInt32Array::from(index_array);
+
+                let arrays = Array::from(&[
+                    packed_vertices.to_variant(), // Vertices
+                    Variant::nil(),               // Normals
+                    Variant::nil(),
+                    Variant::nil(),
+                    Variant::nil(),
+                    Variant::nil(),
+                    Variant::nil(),
+                    Variant::nil(),
+                    Variant::nil(),
+                    Variant::nil(),
+                    Variant::nil(),
+                    Variant::nil(),
+                    packed_indices.to_variant(), // Indices
+                ]);
+
+                let mut output = ArrayMesh::new_gd();
+                godot_print!("go: {}", output.get_surface_count());
+                output.add_surface_from_arrays(PrimitiveType::TRIANGLES, &arrays);
+
+                godot_print!("{}", output.get_surface_count());
+
+                output.to_variant()
             }
-            Err(error) => {
-                godot_print!("{}", error);
-            }
+            Err(_error) => 0.to_variant(),
         }
     }
 }
