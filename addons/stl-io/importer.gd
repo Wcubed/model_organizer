cefebb743fa26c499b1c6a9dcddda62765aefef1
Wybe Westra <wybe@ruurdwestra.nl>
2026-03-10 16:34:12 +0100

    Attempt at rustifying the godot stl import.

--- addons/stl-io/importer.gd
+++ addons/stl-io/importer.gd
@@ -5,4 +5,5 @@ static func LoadFromPath(path: String) -> Variant:
 	:return: ArrayMesh or Int
 	'''
 	var result := StlLoader.load_from_file(path)
+	print(result)
 	return result
--- rust_native/src/lib.rs
+++ rust_native/src/lib.rs
@@ -1,7 +1,11 @@
 use godot::{
-    classes::{ArrayMesh, file_access::ModeFlags, mesh::PrimitiveType},
+    classes::{ArrayMesh, FileAccess, mesh::PrimitiveType},
+    global::Error,
     prelude::*,
 };
+
+const HEADER_BYTES: usize = 80;
+const BYTES_PER_TRIANGLE: usize = 50;
 
 struct RustNativeExtension;
 
@@ -24,71 +28,106 @@ impl StlLoader {
     /// Returns ArrayMesh or an integer to indicate error.
     #[func]
     fn load_from_file(path: GString) -> Variant {
-        let Ok(mut file) = GFile::open(&path, ModeFlags::READ) else {
-            return 0.to_variant();
-        };
-
-        let result: Result<stl_io::IndexedMesh, _> = stl_io::read_stl(&mut file);
-
-        match result {
-            Ok(stl) => {
-                // Each face has 3 vertices, so we know the capacities needed.
-                let mut vertex_array = Vec::with_capacity(stl.vertices.len());
-                let mut normal_array = vec![Vector3::ZERO; stl.vertices.len()];
-                let mut index_array = Vec::with_capacity(stl.faces.len() * 3);
-
-                for vertex in stl.vertices {
-                    vertex_array.push(Vector3::new(vertex[0], vertex[1], vertex[2]));
-                }
-                for face in stl.faces {
-                    let mut indices = face.vertices;
-                    let a = vertex_array[indices[0]];
-                    let b = vertex_array[indices[1]];
-                    let c = vertex_array[indices[2]];
-                    let normal = Vector3::new(face.normal[0], face.normal[1], face.normal[2]);
-
-                    if Plane::from_points(a, b, c).normal.dot(normal) < 0.0 {
-                        // Plane is facing the wrong way.
-                        indices.reverse();
-                    }
-
-                    for index in face.vertices.iter().rev() {
-                        index_array.push(*index as i32);
-                        normal_array[*index] += normal;
-                    }
-                }
-
-                // Normalize the normals.
-                for normal in &mut normal_array {
-                    *normal = normal.normalized();
-                }
-
-                let packed_vertices = PackedVector3Array::from(vertex_array);
-                let packed_normals = PackedVector3Array::from(normal_array);
-                let packed_indices = PackedInt32Array::from(index_array);
-
-                let arrays = Array::from(&[
-                    packed_vertices.to_variant(), // Vertices
-                    packed_normals.to_variant(),  // Normals
-                    Variant::nil(),
-                    Variant::nil(),
-                    Variant::nil(),
-                    Variant::nil(),
-                    Variant::nil(),
-                    Variant::nil(),
-                    Variant::nil(),
-                    Variant::nil(),
-                    Variant::nil(),
-                    Variant::nil(),
-                    packed_indices.to_variant(), // Indices
-                ]);
-
-                let mut output = ArrayMesh::new_gd();
-
-                output.add_surface_from_arrays(PrimitiveType::TRIANGLES, &arrays);
-                output.to_variant()
-            }
-            Err(_error) => 0.to_variant(),
+        let bytes = FileAccess::get_file_as_bytes(&path);
+        if bytes.is_empty() {
+            return FileAccess::get_open_error().to_variant();
         }
+
+        godot_print!("Got file");
+
+        match load_stl_from_buffer(bytes) {
+            Ok(mesh) => mesh.to_variant(),
+            Err(error) => error.to_variant(),
+        }
+    }
+}
+
+/// Rust version of the stl load function from stlio:
+/// https://github.com/onze/godot-stl-io/blob/7700b430a2aac0bab1bee3dcad1ad70aa36e2017/addons/stl-io/importer.gd#L139
+fn load_stl_from_buffer(bytes: PackedByteArray) -> Result<Gd<ArrayMesh>, Error> {
+    let mut mesh = ArrayMesh::new_gd();
+    // Header usually not important, so we skip it.
+    let mut offset = HEADER_BYTES;
+
+    let triangle_count = bytes.decode_u32(offset).corrupt_err()? as usize;
+    offset += 4;
+
+    godot_print!("count: {}", triangle_count);
+
+    // Create the vectors with known capacity, to reduce re-allocation.
+    let mut vertices = PackedVector3Array::new();
+    vertices.resize(triangle_count * 3);
+    let mut normals = PackedVector3Array::new();
+    normals.resize(triangle_count);
+
+    if offset + triangle_count * BYTES_PER_TRIANGLE > bytes.len() {
+        return Err(Error::ERR_FILE_CORRUPT);
+    }
+
+    godot_print!("File size correct");
+
+    for index in 0..triangle_count {
+        let normal = get_vector(&bytes, &mut offset)?;
+        normals[index] = normal;
+
+        let v1 = get_vector(&bytes, &mut offset)?;
+        let v2 = get_vector(&bytes, &mut offset)?;
+        let v3 = get_vector(&bytes, &mut offset)?;
+
+        godot_print!("{} > {}", index * 3, vertices.len());
+
+        if Plane::from_points(v1, v2, v3).normal.dot(normal) > 0.0 {
+            // Face is the right way up.
+            vertices[index * 3] = v1;
+            vertices[(index * 3) + 1] = v2;
+            vertices[(index * 3) + 2] = v3;
+        } else {
+            // Face is upside down.
+            vertices[index * 3] = v3;
+            vertices[(index * 3) + 1] = v2;
+            vertices[(index * 3) + 2] = v1;
+        }
+        godot_print!("{} done", index);
+    }
+
+    let arrays = Array::from(&[
+        vertices.to_variant(), // Vertices
+        normals.to_variant(),  // Normals
+        Variant::nil(),
+        Variant::nil(),
+        Variant::nil(),
+        Variant::nil(),
+        Variant::nil(),
+        Variant::nil(),
+        Variant::nil(),
+        Variant::nil(),
+        Variant::nil(),
+        Variant::nil(),
+        Variant::nil(), // Indices
+    ]);
+    mesh.add_surface_from_arrays(PrimitiveType::TRIANGLES, &arrays);
+
+    godot_print!("{}", mesh.get_surface_count());
+
+    Ok(mesh)
+}
+
+fn get_vector(bytes: &PackedByteArray, offset: &mut usize) -> Result<Vector3, Error> {
+    let vector = Vector3::new(
+        bytes.decode_float(*offset).corrupt_err()?,
+        bytes.decode_float(*offset + 4).corrupt_err()?,
+        bytes.decode_float(*offset + 8).corrupt_err()?,
+    );
+    *offset += 12;
+    Ok(vector)
+}
+
+trait ToCorruptError<T> {
+    fn corrupt_err(self) -> Result<T, Error>;
+}
+
+impl<T> ToCorruptError<T> for Result<T, ()> {
+    fn corrupt_err(self) -> Result<T, Error> {
+        self.map_err(|()| Error::ERR_FILE_CORRUPT)
     }
 }
